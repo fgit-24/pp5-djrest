b1b21158c4b4007c96df02b90b0e51d5395ec5fe
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 18:36:20 +0100

    Add get and post method

--- BlogAPI/backend/views.py
+++ BlogAPI/backend/views.py
@@ -12,13 +12,17 @@ from rest_framework import mixins
 from rest_framework import generics
 
 
+class ArticleList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-
-
-
-
-
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 
