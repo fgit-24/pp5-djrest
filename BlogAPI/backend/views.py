c3f9b2868182a2d644651297e02daba403d5640d
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 17:22:01 +0100

    Utilize @api_view decorator to build GET and POST article view

--- BlogAPI/backend/views.py
+++ BlogAPI/backend/views.py
@@ -5,45 +5,61 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-@csrf_exempt
+
+@api_view(['GET', 'POST'])
 def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
 
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe = False)
+
+
+
+
+
+# @csrf_exempt
+# def article_list(request):
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+
+#         return JsonResponse(serializer.data, safe=False)
     
 
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status = 201)
-        return JsonResponse(serializer.errors, status=400)
+#     elif request.method == "POST":
+#         data = JSONParser().parse(request)
+#         serializer = ArticleSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status = 201)
+#         return JsonResponse(serializer.errors, status=400)
 
 
 
-@csrf_exempt
-def article_details(request, slug):
+# @csrf_exempt
+# def article_details(request, slug):
 
-    try:
-        article = Article.objects.get(slug=slug)
-    except Article.DoesNotExist:
-        return HttpResponse(status=404)
+#     try:
+#         article = Article.objects.get(slug=slug)
+#     except Article.DoesNotExist:
+#         return HttpResponse(status=404)
     
-    if request.method == "GET":
-        serializer = ArticleSerializer(article)
-        return JsonResponse(serializer.data)
+#     if request.method == "GET":
+#         serializer = ArticleSerializer(article)
+#         return JsonResponse(serializer.data)
     
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(article, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
+#     elif request.method == "PUT":
+#         data = JSONParser().parse(request)
+#         serializer = ArticleSerializer(article, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=200)
+#         return JsonResponse(serializer.errors, status=400)
     
-    elif request.method == "DELETE":
-        return HttpResponse(status=204)
+#     elif request.method == "DELETE":
+#         return HttpResponse(status=204)
