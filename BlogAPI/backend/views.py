31d9d0a819fff9b0cc2b99e9439c43c8a803b170
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 21:24:46 +0100

    Set up authenticated imports on views

--- BlogAPI/backend/views.py
+++ BlogAPI/backend/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
