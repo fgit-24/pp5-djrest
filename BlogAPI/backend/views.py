9468cfc510ca856620526f66fb6b51a0e2ef9d0e
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 17:42:31 +0100

    Change JSONResopone to Response, to be compatible with the api_view decorator

--- BlogAPI/backend/views.py
+++ BlogAPI/backend/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -15,10 +15,16 @@ def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return JsonResponse(serializer.data, safe = False)
-
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 
