916b44df804b65a88b13cfc4e3d9facded75b7b0
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 22:27:23 +0100

    Set up allowed hosts

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
