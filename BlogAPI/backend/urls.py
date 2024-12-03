117a07b7987ea96b69129d22638e13fa085a5550
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 18:56:11 +0100

    Adjust urls

--- BlogAPI/backend/urls.py
+++ BlogAPI/backend/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import ArticleList
 
 urlpatterns = [
-    # path('articles/', article_list),
-    # path('articles/<slug:slug>/', article_details),
     path('articles/', ArticleList.as_view())
 ]
