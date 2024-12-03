7946389cef27a78c8388ec29cca9ebabed09d6f5
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 21:29:29 +0100

    Built permissions

--- BlogAPI/backend/views.py
+++ BlogAPI/backend/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthor
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = 'slug'
+
+    permission_classes = [IsAuthenticated,IsAuthor]
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
