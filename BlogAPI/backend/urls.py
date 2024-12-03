5591199f0f33de95654a86fca955c89e8fcf1ad5
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 18:38:20 +0100

    Add url

--- BlogAPI/backend/urls.py
+++ BlogAPI/backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, article_details
+from .views import ArticleList
 
 urlpatterns = [
-    path('articles/', article_list),
-    path('articles/<slug:slug>/', article_details),
+    # path('articles/', article_list),
+    # path('articles/<slug:slug>/', article_details),
+    path('articles/', ArticleList.as_view())
 ]
