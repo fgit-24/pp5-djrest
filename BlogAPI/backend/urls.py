1eef6a1e6d7fddd1e9080392baa518a1e786489f
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-03 19:12:02 +0100

    Change to router urls

--- BlogAPI/backend/urls.py
+++ BlogAPI/backend/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import ArticleList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
+
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
+
 
 urlpatterns = [
-    path('articles/', ArticleList.as_view())
+    path('', include(router.urls))
 ]
