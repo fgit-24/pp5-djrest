a4c9699e0a6e6f6988259e45ae587c3860ed43e8
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-04 12:16:21 +0100

    Add corsheaders to settings

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'dj_rest_auth',
     'django.contrib.sites',
     'allauth',
--- BlogAPI/backend/serializers.py
+++ BlogAPI/backend/serializers.py
@@ -8,21 +8,3 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = '__all__'
 
-# class ArticleSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=200)
-#     description = serializers.CharField()
-#     slug = serializers.SlugField(max_length=200)
-#     published = serializers.DateTimeField(read_only=True)
-
-
-#     def create(self, validated_data):
-#         return Article.objects.create(**validated_data)
-    
-    
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.description = validated_data.get('title', instance.title)
-#         instance.slug = validated_data.get('title', instance.title)
-#         instance.published = validated_data.get('title', instance.title)
-#         instance.save()
-#         return instance
