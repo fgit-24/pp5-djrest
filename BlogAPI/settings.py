83bb23396896074c7c3b43bae15d0c497256a3aa
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-06 18:22:54 +0100

    Set pip version

--- BlogAPI/settings.py
+++ BlogAPI/settings.py
@@ -19,7 +19,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'djrest-f-bfd9b0bcf48e.herokuapp.com', "frontend-f-e4828e1f7975.herokuapp.com", "frontend-f-2bf7aea4b103.herokuapp.com"]
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
