f4091581ab2ca929c4bbdc9894a0376ee5077d80
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-06 11:20:04 +0100

    Set up routes for contact

--- BlogAPI/BlogAPI/urls.py
+++ BlogAPI/BlogAPI/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
+    path('api/', include('backend.urls')),
+    path('api/', include('contact.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
