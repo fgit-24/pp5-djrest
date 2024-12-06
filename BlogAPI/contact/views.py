4dc7242bee56a0d8757cde25eac848b072c42004
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-06 10:41:39 +0100

    Set up ModelViewSet for Contact

--- BlogAPI/contact/views.py
+++ BlogAPI/contact/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response({'message': 'Contact form submitted successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
